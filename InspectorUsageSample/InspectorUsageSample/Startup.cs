082a07e7f7b6f77903183d83fb8ffa44cb23781a
Nachiappan <nachiapan@gmail.com>
2018-12-09 21:31:16 +0530

    Added sample for all scenarios

--- InspectorUsageSample/InspectorUsageSample/Startup.cs
+++ InspectorUsageSample/InspectorUsageSample/Startup.cs
@@ -40,14 +40,13 @@ namespace InspectorUsageSample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseInspector(x =>
-            {
-                x.AddEnvironment("developement");
-                x.AddName("Inspector Usage Sample");
-                x.AddVersion("1.0.0");
-                x.AddKeyValue("Some Key", "Some Value");
-            });
-
+            ConfiguringInspectorExample1(app);
+            //ConfiguringInspectorExample2(app);
+            //ConfiguringInspectorExample3(app);
+            //ConfiguringInspectorExample4(app);
+            //ConfiguringInspectorExample5(app);
+            //ConfiguringInspectorExample6(app);
+            //ConfiguringInspectorExample7(app);
 
             if (env.IsDevelopment())
             {
@@ -69,6 +68,182 @@ namespace InspectorUsageSample
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private void ConfiguringInspectorExample1(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.AddName("Inspector Usage Sample");
+                x.AddVersion("1.0.0");
+                x.AddKeyValue("Some Key", "Some Value");
+            });
+
+            /*
+             * Version endpoint : https://localhost:44397/version
+             *
+             * Version data
+             *{
+             *  "Environment": "development",
+             *  "Name": "Inspector Usage Sample",
+             *  "Version": "1.0.0",
+             *  "Some Key": "Some Value"
+             *}
+             *
+             */
+        }
+
+        private void ConfiguringInspectorExample2(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.AddName("Inspector Usage Sample");
+                x.AddVersion("1.0.0");
+                x.AddKeyValue("Some Key", "Some Value");
+                x.AddConfigurationSection(Configuration, "ConfigurationData1");
+            });
+
+            /*
+             * Version endpoint : https://localhost:44397/version
+             *
+             * Version data
+                {
+                  "Environment": "development",
+                  "Name": "Inspector Usage Sample",
+                  "Version": "1.0.0",
+                  "Some Key": "Some Value",
+                  "Property1": "Value1"
+                }
+             *
+             */
+        }
+
+        private void ConfiguringInspectorExample3(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.AddName("Inspector Usage Sample");
+                x.AddVersion("1.0.0");
+                x.AddKeyValue("Some Key", "Some Value");
+                x.AddConfigurationSection(Configuration, "Node1:Node2:1");
+            });
+
+            /*
+             * Version endpoint : https://localhost:44397/version
+             *
+             * Version data
+                {
+                  "Environment": "development",
+                  "Name": "Inspector Usage Sample",
+                  "Version": "1.0.0",
+                  "Some Key": "Some Value",
+                  "Prop1": "Value1",
+                  "Prop2": "Value2"
+                }
+             *
+             */
+        }
+
+        private void ConfiguringInspectorExample4(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.SetBaseEndpoint("/inspector/ins");
+            });
+
+            /*
+             * Version endpoint : https://localhost:44397/inspector/ins/version
+             *
+             * Version data
+                {
+                  "Environment": "development",
+                }
+             *
+             */
+        }
+
+        private void ConfiguringInspectorExample5(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.AuthenticateWith("one" , "two");
+            });
+
+            /*
+             * Version endpoint (guarded) : https://localhost:44397/version
+             * Expects in header (key:value)->(wondertools-authorization:one) or (wondertools-authorization:two)
+             * Version Login endpoint exposed at : https://localhost:44397/version-login
+             * This provides UI for typing in your password
+             *
+             * Version data
+                {
+                  "Environment": "development",
+                }
+             *
+             */
+        }
+
+        private void ConfiguringInspectorExample6(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.SetBaseEndpoint("/inspector");
+                x.AuthenticateWith((password) =>
+                {
+                    if ("Monday" == password) return true;
+                    if ("Tuesday" == password) return true;
+                    return false;
+                });
+            });
+
+            /*
+             * Version endpoint (guarded) : https://localhost:44397/inspector/version
+             * Expects in header (key:value)->(wondertools-authorization:Monday) or (wondertools-authorization:Tuesday)
+             * Version Login endpoint exposed at : https://localhost:44397/inspector/version-login
+             * This provides UI for typing in your password
+             *
+             * Version data
+                {
+                  "Environment": "development",
+                }
+             *
+             */
+        }
+
+        private void ConfiguringInspectorExample7(IApplicationBuilder app)
+        {
+            app.UseInspector(x =>
+            {
+                x.AddEnvironment("development");
+                x.SetBaseEndpoint("/inspector");
+                x.AuthenticateWith((password) =>
+                {
+                    if ("Monday" == password) return true;
+                    if ("Tuesday" == password) return true;
+                    return false;
+                });
+                x.EnableCors();
+            });
+
+            /*
+             * Version endpoint (guarded) : https://localhost:44397/inspector/version
+             * Expects in header (key:value)->(wondertools-authorization:Monday) or (wondertools-authorization:Tuesday)
+             * ***Cross Origin request to the version endpoint are not allowed 
+             * Version Login endpoint exposed at : https://localhost:44397/inspector/version-login
+             * This provides UI for typing in your password
+             *
+             * Version data
+                {
+                  "Environment": "development",
+                }
+             *
+             */
         }
     }
 }
